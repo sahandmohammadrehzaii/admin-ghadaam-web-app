b88ad91f3585d43ed57780a12056efd41dfa03c7
sahand <mohammadrehzaiisahand@gmail.com>
2024-06-12 18:31:40 +0330

    hello people

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  'mongodb://root:FfezyuEXFiWwoPgjdNMBgvRx@kilimanjaro.liara.cloud:32862/my-app?authSource=admin&replicaSet=rs0&directConnection=true';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
